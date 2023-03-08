81920c3f7e23bbd9ad5f821c96eb9b4ae127d3ca
27010-17-04 <27010-17-04@exchange.ad.afpanet>
2023-03-08 08:05:00 +0100

    premier commit

---
+++ cours_js/index.js
@@ -0,0 +1,8 @@
+
+function myFunc() {
+    var num1 = parseInt(prompt("Enter a number: "));
+    var num2 = parseInt(prompt("Enter another number: ")); 
+    var sum = num1 + num2;
+    alert("The sum of the two numbers is: " + sum);
+}
+    
